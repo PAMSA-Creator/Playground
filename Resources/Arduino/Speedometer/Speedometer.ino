2606ad89883c3e985a5fd470528e2780c7c1329c
Vincent Villatte <vvillatte@vvillatte.net>
2021-05-14 00:14:31 +1000

    Added third method
    Third method measures the time difference between two interrupts to derive the frequency, hence RPM

--- Resources/Arduino/Speedometer/Speedometer.ino
+++ Resources/Arduino/Speedometer/Speedometer.ino
@@ -1,14 +1,19 @@
-#define _TEST       // Comment out if using external input on interruptPin
-#define METHOD 2    // METHOD can be either 1 or 2 only
+//#define _TEST       // Comment out if using external input on interruptPin
+#define METHOD 3    // METHOD can be either 1 or 2 only
 
 unsigned long newTime = 0;
 unsigned long oldTime = 0;
 unsigned long elapsedTime = 0;
+double period = 0.0;
+double frequency = 0.0;
 unsigned int rpm = 0;
 const unsigned int pulsesPerTurn = 20;
-const int interruptPin = 2;
+const int interruptPin = 3;
 const int interruptNumber = digitalPinToInterrupt(interruptPin);
 volatile unsigned int pulses = 0;
+volatile unsigned long ISRTime = 0;
+volatile unsigned long previousISRTime = 0;
+volatile boolean isReady = false;
 
 #if defined(_TEST)
 // Uno, Nano, Mini 3, 5, 6, 9, 10, 11  490 Hz (pins 5 and 6: 980 Hz)
@@ -22,8 +27,20 @@ const int highFrequencyPin = 6;
  */
 void count(void){
   // Increment the pulse counter
+  // set the period between the last two pulses
+  // Set the ready flag
+#if (1 == METHOD)
   pulses++;
+#elif (2 == METHOD)
+  pulses++;
+#elif (3 == METHOD)
+    previousISRTime = ISRTime;
+    ISRTime = micros();
+    pulses++;
+    isReady = (2 > pulses) ? false : true;
+#endif
 }
+
 
 /*
  * The method1() function monitors the time elapsed between each loop
@@ -76,25 +93,69 @@ void method1(){
  * This second method is may be used if proven to return better results than method1()
  */
 void method2(){
-  // Start by resetting the counter
+  // Start by resetting the counter and timer
   pulses = 0;
 
   // Attach/enable the interrupt
   attachInterrupt(interruptNumber, count, FALLING);
 
-  // Wait 1 second exactly
+  // Wait 1 second
   delay(1000);
 
   // Disable the interrupt
   detachInterrupt(interruptNumber);
 
   // Calculate the result
-//  rpm = pulses * 60 / pulsesPerTurn;
-  rpm = pulses;
+  rpm = pulses * 60 / pulsesPerTurn;
 
   // Output the result on the terminal
   Serial.print("RPM =  ");
   Serial.println(rpm);
+}
+
+/*
+ * The method3() function works ...
+ * time is taken inside the ISR ...
+ */
+void method3(){
+  // Start by resetting the counter and timer
+  pulses = 0;
+  isReady = false;
+
+  // Attach/enable the interrupt
+  attachInterrupt(interruptNumber, count, FALLING);
+
+  // Wait until ready
+  delay(1000);
+  while(!isReady);
+
+  // Disable the interrupt
+  detachInterrupt(interruptNumber);
+
+  // Calculate the result
+  // Calculate the pulses frequency (inverse of the time between two pulses)
+  // Get the time (in microseconds) between the last two interrupts
+  elapsedTime = ISRTime - previousISRTime;
+  if(12 >= elapsedTime){
+    //Serial.println("Time ERROR");
+  }
+  else{
+    // Convert elapsedTime from microseconds to seconds
+    period = (double)elapsedTime / 1000000.0;
+    // Calculate the frequency 1 / (deltaT / pulses)
+    frequency = 1.0 / period;
+    rpm = frequency * 60.0 / pulsesPerTurn;
+  
+    // Output the result on the terminal
+    Serial.print("elapsedTime =  ");
+    Serial.println(elapsedTime);
+    Serial.print("period =  ");
+    Serial.println(period);
+    Serial.print("Frequency =  ");
+    Serial.println(frequency);
+    Serial.print("RPM =  ");
+    Serial.println(rpm);
+  }
 }
 
 /*
@@ -130,5 +191,7 @@ void loop() {
   method1();
 #elif (2 == METHOD)
   method2();
+#elif (3 == METHOD)
+  method3();
 #endif
 }
